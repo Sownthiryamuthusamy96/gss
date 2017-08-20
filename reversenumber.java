ba70c0baf5780a3d8618c8590a5133473cc75221
Sownthiryamuthusamy96 <sownthiryamuthusamy@gmail.com>
2017-08-20 20:29:04 +0530

    Create reversenumber.java

---
+++ reversenumber.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+public class reversenumber
+{
+	public static void main(String[] args) throws NumberFormatException, IOException
+		{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		String n=br.readLine();
+		System.out.println(n);
+		StringBuffer buf=new StringBuffer(n);
+		String ss=buf.reverse().toString();
+		int n1=Integer.parseInt(ss);
+		System.out.println(n1);
+		
+		
+		}
